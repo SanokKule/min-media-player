6fad20b232177eac8c6c8b33305e1c0b6cf98c65
SanokKule <sanokkule@yandex.ru>
2023-04-13 00:20:41 +0400

    hide play button when no access(0.44); v1.03

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
 exports.description = "minimalistic media player"
-exports.version = 1.02
+exports.version = 1.03
 exports.apiRequired = 8
 exports.repo = "SanokKule/min-media-player"
 exports.frontend_css = 'style.css'
