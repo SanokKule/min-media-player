21997b339a8f77884b5301ff4d7d32cbe48ed859
SanokKule <sanokkule@yandex.ru>
2024-10-21 03:39:05 +0400

    Revert "remove ability to focus on media part; v1.192"
    This reverts commit f9ee3b1d2752a2c32c94aa70ee9fecc7c8d60e03.

--- dist/public/main.js
+++ dist/public/main.js
@@ -1,4 +1,4 @@
-console.log("HFS plugin: min-media-player v1.192 by SanokKule")
+console.log("HFS plugin: min-media-player v1.19 by SanokKule")
 
 const MMP = {
 	cfg: HFS.getPluginConfig(),
@@ -62,7 +62,7 @@ const { h, t } = HFS
 const cfg = HFS.getPluginConfig()
 audioHtml = `
 <div id='mmp-audio' class='mmp'>
-	<audio class='mmp-media' controls controlslist='nodownload' tabindex='-1'>
+	<audio class='mmp-media' controls controlslist='nodownload'>
 	</audio>
 	<div class='mmp-controls'>
 		<span class='mmp-title'>
@@ -74,7 +74,7 @@ audioHtml = `
 
 videoHtml = `
 <div id='mmp-video' class='mmp'>
-	<video class='mmp-media' ${cfg.start_video_with.toString().replace(',', ' ')} controls controlslist='nodownload' tabindex='-1'>
+	<video class='mmp-media' ${cfg.start_video_with.toString().replace(',', ' ')} controls controlslist='nodownload'>
 	</video>
 	<div class='mmp-controls'>
 		<span class='mmp-title'>
@@ -88,7 +88,7 @@ videoHtml = `
 
 imageHtml = `
 <div id='mmp-image' class='mmp'>
-	<img title='MMP image viewer' class='mmp-media' tabindex='-1'>
+	<img title='MMP image viewer' class='mmp-media'>
 	</img>
 	<div class='mmp-controls'>
 		<span class='mmp-title'>
